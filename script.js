7c7f4fb07b29d2e59aeafcd57fe60523e6a7cc97
Krish Saluja <krishsaluja297@gmail.com>
2024-02-05 22:47:14 +0530

    added functionality to the mini-program

---
+++ script.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById("btn");
+
+
+btn.addEventListener("click", function() {
+    document.body.style.backgroundColor = "black";
+    })
+
+
+btn.addEventListener("dblclick", ()=> {
+    document.body.style.backgroundColor= "white";
+})
