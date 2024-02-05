cb10a19642972c06a8478cd0048503e1f8747606
Krish Saluja <krishsaluja297@gmail.com>
2024-02-05 22:50:08 +0530

    now logs into console what mode you are in

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
     document.body.style.backgroundColor = "black";
+    console.log("Changed to Dark Mode")
     })
 
 
 btn.addEventListener("dblclick", ()=> {
     document.body.style.backgroundColor= "white";
+    console.log("Changed to Light Mode")
 })
